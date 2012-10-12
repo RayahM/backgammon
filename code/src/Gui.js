b2be4a9c453a0467a01a4a90aca46b7623d9dcbf
Siim-Toomas Marran <siimtoomas.marran@gmail.com>
2012-10-12 14:05:08 +0300

    Dice roll

--- code/src/Gui.js
+++ code/src/Gui.js
@@ -1,3 +1,9 @@
+var whiteDiceNrs = ["white-one", "white-two","white-three","white-four","white-five","white-six" ];
+var brownDiceNrs = ["brown-one", "brown-two","brown-three","brown-four","brown-five","brown-six" ];
+var currentWhiteNr = whiteDiceNrs[0];
+var currentBrownNr = brownDiceNrs[2];
+
+
 $(function(){
 	
 	$('#menu').click(function() {		
@@ -10,6 +16,8 @@ $(function(){
                     $( this ).dialog( "close" );
                     
                     //todo start new game
+                    
+                    
                 },
                 Cancel: function() {
                     $( this ).dialog( "close" );
@@ -33,15 +41,44 @@ $(function(){
 	});
 
 	$('#send').click(function() {
-		  consoleAddMessage($('#chat_input').val());
-		  $('#chat_input').val('');
+		if($('#chat_input').val() != ""){
+			consoleAddMessage($('#chat_input').val());			  
+			$('#chat_input').val('');
+		}
 	});
+	
+	$('#whiteDiceNr').click(function() {
+		
+		  //White dice clicked
+		  rollDice();
+		 });
+	
+	 $('#brownDiceNr').click(function() {
+		 
+	  //Brown dice clicked
+	  rollDice();
+	 });
 });
 
 
 function consoleAddMessage(message){
 	$('#log').append(message+"<br />");
-	$('#log').animate({scrollTop:$("#log")[0].scrollHeight - $("#log").height()},1000,function() {
-	    // Animation complete.
-	});
+	$('#log').animate({scrollTop:$("#log")[0].scrollHeight - $("#log").height()},1000,function() {});
+}
+
+function rollDice() {
+	 for(var i=0; i < 5; i++) {
+	  var newWhiteNr = Math.floor((Math.random()*6));
+	  var newBrownNr = Math.floor((Math.random()*6));
+	  console.log('newWhiteNr: ' + newWhiteNr + " " + whiteDiceNrs[newWhiteNr]);
+	  console.log('newBrownNr: ' + newBrownNr + " " + brownDiceNrs[newBrownNr]);
+	  if(currentWhiteNr != whiteDiceNrs[newWhiteNr]) {
+	   $('#whiteDiceNr').switchClass(currentWhiteNr, whiteDiceNrs[newWhiteNr], 125);
+	  }
+	  if(currentBrownNr != brownDiceNrs[newBrownNr]) {
+	   $('#brownDiceNr').switchClass(currentBrownNr, brownDiceNrs[newBrownNr], 125);
+	  }
+	  currentWhiteNr = whiteDiceNrs[newWhiteNr];
+	  currentBrownNr = brownDiceNrs[newBrownNr];
+	 }
 }
