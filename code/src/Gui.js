f221cc88ccb9ea2762aa6b6ee9657cb74dd49cad
Indrek Juhkam <indrek@urgas.eu>
2012-10-12 18:27:01 +0300

    Integrate initial dice rolling with actual game

--- code/src/Gui.js
+++ code/src/Gui.js
@@ -1,24 +1,23 @@
-var whiteDiceNrs = [ "white-one", "white-two", "white-three", "white-four",
-    "white-five", "white-six" ];
-var brownDiceNrs = [ "brown-one", "brown-two", "brown-three", "brown-four",
-    "brown-five", "brown-six" ];
-var currentWhiteNr = whiteDiceNrs[0];
-var currentBrownNr = brownDiceNrs[2];
+var whiteDiceNrs = [
+  "white-one", "white-two", "white-three", "white-four", "white-five", "white-six"
+];
+var brownDiceNrs = [
+  "brown-one", "brown-two", "brown-three", "brown-four", "brown-five", "brown-six"
+];
 
 var PLAYER1 = new Player("Player1");
 var PLAYER2 = new Player("Player2");
 var GAME = new Game();
 
 $(function() {
+  showStartUpWindow();
 
-  startUpWindow();
-
-  $('#player1Input').click(function() {
-    $('#player1Input').val("");
+  $("#player1Input").click(function() {
+    $("#player1Input").val("");
   });
 
-  $('#player2Input').click(function() {
-    $('#player2Input').val("");
+  $("#player2Input").click(function() {
+    $("#player2Input").val("");
   });
 
   $('#menu').click(function() {
@@ -59,25 +58,9 @@ $(function() {
       $('#chat_input').val('');
     }
   });
-
-  $('#whiteDiceNr1').click(function() {
-    rollWhiteDice('#whiteDiceNr1');
-  });
-
-  $('#whiteDiceNr2').click(function() {
-    rollWhiteDice('#whiteDiceNr2');
-  });
-
-  $('#brownDiceNr1').click(function() {
-    rollBrownDice('#brownDiceNr1');
-  });
-
-  $('#brownDiceNr2').click(function() {
-    rollBrownDice('#brownDiceNr2');
-  });
 });
 
-function startUpWindow() {
+function showStartUpWindow() {
   $("#dialog-modal").dialog({
     height : 250,
     modal : true,
@@ -104,6 +87,49 @@ function startNewGame() {
   $("#player2Name").text(PLAYER2.name);
 
   GAME.start();
+
+  showWhiteDice(GAME.diceRoller.firstValue);
+  showBrownDice(GAME.diceRoller.secondValue);
+}
+
+// value - 1 to 6
+function showWhiteDice(value) {
+  var dice = $("#whiteDiceNr1");
+  dice.parent().show();
+  animateDice(dice, whiteDiceNrs, value);
+}
+
+// value - 1 to 6
+function showBrownDice(value) {
+  var dice = $("#brownDiceNr1");
+  dice.parent().show();
+  animateDice(dice, brownDiceNrs, value);
+}
+
+// dice - dice jquery object
+// numbers - array of number classes
+// value - final value 1 to 6
+function animateDice(dice, numbers, value) {
+  dice.removeClass().addClass("dice");
+
+  var lastClass = null;
+  var count = 0;
+  var nrGenerator = new DiceNumberGenerator();
+
+  var showRandomNr = function() {
+    var randomNr = nrGenerator.generate() - 1;
+    dice.switchClass(lastClass, numbers[randomNr], 80, function() {
+      if (count <= 8) {
+        lastClass = numbers[randomNr];
+        showRandomNr();
+      } else {
+        dice.switchClass(lastClass, numbers[value - 1]);
+      }
+    });
+    count++;
+  }
+
+  showRandomNr();
 }
 
 function consoleAddMessage(message) {
@@ -114,102 +140,14 @@ function consoleAddMessage(message) {
   });
 }
 
-function rollDice(string) {
-  for ( var i = 0; i < 5; i++) {
-    var newWhiteNr = Math.floor((Math.random() * 6));
-    var newBrownNr = Math.floor((Math.random() * 6));
-    console.log('newWhiteNr: ' + newWhiteNr + " "
-        + whiteDiceNrs[newWhiteNr]);
-    console.log('newBrownNr: ' + newBrownNr + " "
-        + brownDiceNrs[newBrownNr]);
-    if (currentWhiteNr != whiteDiceNrs[newWhiteNr]) {
-      $('#whiteDiceNr1').switchClass(currentWhiteNr,
-          whiteDiceNrs[newWhiteNr], 125);
-    }
-    if (currentBrownNr != brownDiceNrs[newBrownNr]) {
-      $('#brownDiceNr1').switchClass(currentBrownNr,
-          brownDiceNrs[newBrownNr], 125);
-    }
-    currentWhiteNr = whiteDiceNrs[newWhiteNr];
-    currentBrownNr = brownDiceNrs[newBrownNr];
-  }
-}
-
-function rollWhiteDice(string, value) {
-  for ( var i = 0; i < 5; i++) {
-    var newWhiteNr = Math.floor((Math.random() * 6));
-    console
-        .log(string + ': ' + newWhiteNr + " "
-            + whiteDiceNrs[newWhiteNr]);
-    if (currentWhiteNr != whiteDiceNrs[newWhiteNr]) {
-      $(string)
-          .switchClass(currentWhiteNr, whiteDiceNrs[newWhiteNr], 125);
-    }
-    currentWhiteNr = whiteDiceNrs[newWhiteNr];
-  }
-  if (currentWhiteNr != whiteDiceNrs[value]) {
-    $(string).switchClass(currentWhiteNr, whiteDiceNrs[value], 125);
-  }
-}
-
-function rollBrownDice(string, value) {
-  for ( var i = 0; i < 5; i++) {
-    var newBrownNr = Math.floor((Math.random() * 6));
-    console
-        .log(string + ': ' + newBrownNr + " "
-            + brownDiceNrs[newBrownNr]);
-    if (currentBrownNr != brownDiceNrs[newBrownNr]) {
-      $(string)
-          .switchClass(currentBrownNr, brownDiceNrs[newBrownNr], 125);
-    }
-    currentBrownNr = brownDiceNrs[newBrownNr];
-  }
-  if (currentBrownNr != brownDiceNrs[value]) {
-    $(string).switchClass(currentBrownNr, brownDiceNrs[value], 125);
-  }
-}
-
-function startGameRolling(whiteValue, brownValue) {
-  $("#game-board")
-      .append(
-          '<div id="whiteDiceNr1Container" class="dice dice-white"><div id="whiteDiceNr1" class="number white-one"></div></div>');
-  $("#game-board")
-      .append(
-          '<div id="brownDiceNr1Container" class="dice dice-brown"><div id="brownDiceNr1" class="number brown-three"></div></div>');
-  rollWhiteDice('#whiteDiceNr1', whiteValue);
-  rollBrownDice('#brownDiceNr1', brownValue);
-}
-
 function removeStartingDice() {
   $("#whiteDiceNr1Container").remove();
   $("#brownDiceNr1Container").remove();
 }
 
-function whiteRolling(first, second) {
-  $("#game-board")
-      .append(
-          '<div id="whiteDiceNr1Container" class="dice dice-white"><div id="whiteDiceNr1" class="number white-one"></div></div>');
-  $("#game-board")
-      .append(
-          '<div id="whiteDiceNr2Container" class="dice dice-white"><div id="whiteDiceNr2" class="number white-one"></div></div>');
-  rollWhiteDice('#whiteDiceNr1', first);
-  rollWhiteDice('#whiteDiceNr2', second);
-}
-
 function removeWhiteDice() {
   $("#whiteDiceNr1Container").remove();
   $("#whiteDiceNr2Container").remove();
-}
-
-function brownRolling(first, second) {
-  $("#game-board")
-      .append(
-          '<div id="brownDiceNr1Container" class="dice dice-brown"><div id="brownDiceNr1" class="number brown-three"></div></div>');
-  $("#game-board")
-      .append(
-          '<div id="brownDiceNr2Container" class="dice dice-brown"><div id="brownDiceNr2" class="number brown-three"></div></div>');
-  rollBrownDice('#brownDiceNr1', first);
-  rollBrownDice('#brownDiceNr2', second);
 }
 
 function removeBrownDice() {
